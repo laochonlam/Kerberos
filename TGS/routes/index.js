904c1e26af43633b02f0189d7df6087b13faf49d
LaM <laochanlam@gmail.com>
2016-12-13 02:37:47 +0800

    established two server

---
+++ TGS/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.sendFile(path.join(__dirname, '../view', 'index.html'));
+});
+
+
+
+module.exports = router;
